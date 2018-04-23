166acc470ee04ece001baac257b5dec2fad5143c
ankur8931 <ankurchaudhary8931@gmail.com>
2018-04-23 02:27:23 -0700

    changes values for vulExists based on successor nodes, code updated.

--- code/README.md
+++ code/README.md
@@ -2,3 +2,6 @@
 customtopo.py - This code is used for generation of mininet network with large number of VMs on a flat network
 managed by a POX SDN controller
 
+- Install cve-search to look for cvss score of the vulnerabilities.
+- Install mongo db on ubuntu
+- Follow cve-search README.md
--- code/bwcentral.py
+++ code/bwcentral.py
@@ -14,7 +14,7 @@ class BWCentral:
 
         self.G = nx.DiGraph()
         self.SG = nx.DiGraph()
-        self.tree = ET.parse('/home/snac/ESORICS/mtd/data/ag-files/15/AttackGraph.xml')
+        self.tree = ET.parse('/home/snac/ESORICS/mtd/data/ag-files/14/AttackGraph.xml')
         self.root = self.tree.getroot()
 
     def createGraph(self):
@@ -23,7 +23,8 @@ class BWCentral:
 
         for arc in arcs.findall('arc'):
 
-            self.G.add_edge(int(arc.find('src').text), int(arc.find('dst').text))
+            self.G.add_edge(int(arc.find('dst').text), int(arc.find('src').text))
+
         bw_central = nx.betweenness_centrality(self.G, normalized=False)
 
         bw_min = min(list(bw_central.values()))
@@ -31,8 +32,47 @@ class BWCentral:
 
         for k,v in bw_central.items():
 
-            bw_central[k] = round(((bw_central[k] - bw_min) / (bw_max - bw_min)),3)
-            
+            bw_central[k] = round(((bw_central[k] - bw_min) / (bw_max - bw_min)),3)            
+        
+        ###Check attributes of the node ID####
+
+        for node in self.G.nodes:
+           
+            for vertex in self.root.find('vertices'):
+
+                if int(vertex.find('id').text) == int(node):
+
+                   if 'vulExists' in vertex.find('fact').text:
+
+                       self.G.node[node]['type'] = 'vulExists'
+
+                   elif 'netAccess' in vertex.find('fact').text:
+
+                       self.G.node[node]['type'] = 'netAccess'
+                
+                   elif 'execCode' in vertex.find('fact').text:
+
+                       self.G.node[node]['type'] = 'execCode'
+
+                   else:
+
+                       self.G.node[node]['type'] = 'n-a'
+
+        #####Check for successor of a given node####
+
+        for node in self.G.nodes:
+
+            if self.G.node[node]['type'] == 'vulExists':
+
+                succNodes = list(nx.nodes(nx.dfs_tree(self.G, node)))
+
+                for sNode in succNodes:
+
+                    if self.G.node[sNode]['type'] == 'execCode' or self.G.node[sNode]['type'] == 'netAccess':
+
+                       bw_central[node] = bw_central[sNode]
+
+
         print(bw_central)
 
 
